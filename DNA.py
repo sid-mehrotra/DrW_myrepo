e86d18e64d0b58a4871128953772ce1691f443a8
Sid Mehrotra <smehrotra1@luc.edu>
2023-02-17 22:32:14 +0000

    2 liner

--- DNA.py
+++ DNA.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 s = open('rosalind_dna.txt','r').read()
-for n in ["A","C","G","T"]:
-    print(s.count(n), end=' ')
+print(s.count("A"), s.count("G"), s.count("C"), s.count("T"))
